ad316a37cdcba6ac4136261d424075a07d04937d
sachemmark <sachemmark@gmail.com>
2015-03-30 13:17:35 +0300

    Initial ball game commit

---
+++ MiniGames Ultra/Ball.swift
@@ -0,0 +1,17 @@
+//
+//  Ball.swift
+//  MiniGames Ultra
+//
+//  Created by Mark Yankovskiy on 30/03/15.
+//  Copyright (c) 2015 TheUnbelievable. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import Cocoa
+
+
+
+class BallGame: NSViewController {
+    
+}
